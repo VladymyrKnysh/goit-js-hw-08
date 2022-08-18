9e2f5e78df185f548a3810fcf194c3b1db6d581c
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-18 15:27:10 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
 
 
+ const markup = createMarkup(galleryItems)
+
+function createMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+   return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+  })
+    .join('')  
+}
+const list = document.querySelector('.gallery')
+list.insertAdjacentHTML('afterbegin', markup)
+
+const lightbox = new SimpleLightbox('.gallery a', {    
+    'captionsData': 'alt',
+    'captionPosition': 'bottom',
+    'captionDelay': 250 
+    
+});
