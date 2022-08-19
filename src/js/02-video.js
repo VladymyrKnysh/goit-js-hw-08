e65c00f2938da520bffa46102a153ec504f6f94b
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-19 14:25:32 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
 
+import Player from "@vimeo/player";
+
+const iframe = document.querySelector('iframe')
+
+const player = new Player(iframe);
+
+ player.on('timeupdate', function(data) {
+        localStorage.setItem('videoplayer-current-time', data.seconds)
+ });
+ let currentTime = localStorage.getItem('videoplayer-current-time')
+    
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+    
