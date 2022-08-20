4b72552f9251cc49b2a75f4a1ed6e82336f1bd87
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-20 11:08:34 +0300

    task2-correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,20 @@
 
 import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe')
 
 const player = new Player(iframe);
 
- player.on('timeupdate', function(data) {
-        localStorage.setItem('videoplayer-current-time', data.seconds)
- });
- let currentTime = localStorage.getItem('videoplayer-current-time')
-    
-player.setCurrentTime(currentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+const STORAGE_KEY = 'videoplayer-current-time'
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(+localStorage.getItem(STORAGE_KEY))
+
+player.on('timeupdate', throttle(onTimeupdate, 1000))
+ 
+function onTimeupdate(params) {
+   localStorage.setItem(STORAGE_KEY, params.seconds) 
+}
+        
+
     
